b3fea7f81374aa123bb41e4d30ae91a0c4aefbc7
thanhln <lenhatthanh100@gmail.com>
2020-12-14 12:54:13 +0700

    First commit

---
+++ src/main/java/lang_market/backend/LangMarketBackendApplication.java
@@ -0,0 +1,13 @@
+package lang_market.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangMarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangMarketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lang_market/backend/LangMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lang_market.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
