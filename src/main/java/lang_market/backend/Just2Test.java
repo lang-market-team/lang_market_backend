1bd4bc344a94a93568a6f6b11f853daa8fc2570d
thanhln <lenhatthanh100@gmail.com>
2020-12-14 13:00:28 +0700

    Add simple api

---
+++ src/main/java/lang_market/backend/Just2Test.java
@@ -0,0 +1,14 @@
+package lang_market.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class Just2Test {
+    @GetMapping
+    Integer login(){
+        return 1;
+    }
+}
