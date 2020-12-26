d9a0fa7de70282234cc6519995c2599b3d10f8c2
thanhln <lenhatthanh100@gmail.com>
2020-12-27 03:50:09 +0700

    Add api for order

--- src/main/java/lang_market/backend/order/OrderService.java
+++ src/main/java/lang_market/backend/order/OrderService.java
@@ -14,23 +14,31 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
-    public Integer createNewOrder(Order order) {
-        return orderRepository.createNewOrder(order.getTotal_price(), order.getCreate_time(), order.getDelivery_address(), order.getId_buyer());
+    public Order createNewOrder(OrderFull orderFull) {
+        return orderRepository.createNewOrder(orderFull.getOrder(), orderFull.getOrderDetailList());
     }
 
     public List<Order> getAllOrderOfBuyer(Integer id_buyer) {
         return orderRepository.getAllOrderOfBuyer(id_buyer);
     }
 
+    public List<Order> getAllOrderOfSeller(Integer id_seller) {
+        return orderRepository.getAllOrderOfSeller(id_seller);
+    }
+
     public List<OrderDetail> getDetailOfOrder(Integer id_order) {
         return orderRepository.getDetailOfOrder(id_order);
     }
 
-    public Order getStatusOfOrder(Integer id_order) {
-        return orderRepository.getStatusOfOrder(id_order);
+    public Order getInformationOfOrder(Integer id_order) {
+        return orderRepository.getInformationOfOrder(id_order);
     }
 
     public Order updateStatusOfOrder(Order order) {
         return orderRepository.updateStatusOfOrder(order.getId_order(), order.getStatus_order());
+    }
+
+    public Order updateDeliveryTimeOfOrder(Order order) {
+        return orderRepository.updateDeliveryTimeOfOrder(order.getId_order(), order.getDelivery_time());
     }
 }
