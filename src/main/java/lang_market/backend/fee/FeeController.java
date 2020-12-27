498c15147fb912a811d943e1b095e27eb426923c
thanhln <lenhatthanh100@gmail.com>
2020-12-27 18:58:33 +0700

    Add api for fee

--- src/main/java/lang_market/backend/fee/FeeController.java
+++ src/main/java/lang_market/backend/fee/FeeController.java
@@ -1,4 +1,52 @@
 package lang_market.backend.fee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/fee")
 public class FeeController {
+    private final FeeService feeService;
+
+    public FeeController(FeeService feeService) {
+        this.feeService = feeService;
+    }
+
+    @RequestMapping("paid")
+    @GetMapping
+    public List<Fee> getAllPaidFee() {
+        return feeService.getAllPaidFee();
+    }
+
+    @RequestMapping("unpaid")
+    @GetMapping
+    public List<Fee> getAllUnpaidFee() {
+        return feeService.getAllUnpaidFee();
+    }
+
+    @RequestMapping("paid/id_seller={id_seller}")
+    @GetMapping
+    public  List<Fee> getAllPaidFeeOfSeller(@PathVariable("id_seller") Integer id_seller) {
+        return feeService.getAllPaidFeeOfSeller(id_seller);
+    }
+
+    @RequestMapping("unpaid/id_seller={id_seller}")
+    @GetMapping
+    public  List<Fee> getAllUnpaidFeeOfSeller(@PathVariable("id_seller") Integer id_seller) {
+        return feeService.getAllUnpaidFeeOfSeller(id_seller);
+    }
+
+    @RequestMapping("create")
+    @PostMapping
+    public Integer createNewFee(@RequestBody Fee fee) {
+        return feeService.createNewFee(fee);
+    }
+
+    @RequestMapping("pay")
+    @PostMapping
+    public Integer payFeeForMonth(@RequestBody Fee fee) {
+        return feeService.payFeeForMonth(fee);
+    }
 }
--- src/main/java/lang_market/backend/fee/FeeRepository.java
+++ src/main/java/lang_market/backend/fee/FeeRepository.java
@@ -31,10 +31,28 @@ public class FeeRepository {
     }
 
     public List<Fee> getAllPaidFeeOfSeller(Integer id_seller) {
-        final String sql = "SELECT * from order_langmarket WHERE id_buyer = ?";
+        final String sql = "SELECT * from fee_langmarket WHERE paid_fee = true AND id_seller = ?";
         return  jdbcTemplate.query(
                 sql,
                 new Object[]{id_seller},
                 new BeanPropertyRowMapper<>(Fee.class));
+    }
+
+    public List<Fee> getAllUnpaidFeeOfSeller(Integer id_seller) {
+        final String sql = "SELECT * from fee_langmarket WHERE paid_fee = false AND id_seller = ?";
+        return  jdbcTemplate.query(
+                sql,
+                new Object[]{id_seller},
+                new BeanPropertyRowMapper<>(Fee.class));
+    }
+
+    public Integer createNewFee(Integer month_fee, Integer fee, Integer id_order, Integer id_seller) {
+        final String sql = "INSERT INTO fee_langmarket (month_fee, fee, id_order, id_seller) VALUES (?, ?, ?, ?)";
+        return jdbcTemplate.update(sql, month_fee, fee, id_order, id_seller);
+    }
+
+    public Integer payFeeForMonth(Integer month_fee, Integer id_seller) {
+        final String sql = "UPDATE fee_langmarket SET paid_fee = true WHERE month_fee = ? AND id_seller = ?";
+        return jdbcTemplate.update(sql, month_fee, id_seller);
     }
 }
